55331a6f845815bf6a43565711911acbe171dc12
Kevin Gruich <kgruich@gmail.com>
2020-12-20 15:22:28 -0500

    added image sfx

--- script.js
+++ script.js
@@ -14,23 +14,30 @@ for (i = 0; i < coll.length; i++) {
     });
 }
 
+var crackTime = new Audio('sounds/crackTime.mp3')
+var pogSFX = new Audio('sounds/jermaPog.mp3')
+var monkaS = new Audio('sounds/monkaS.mp3')
+var noLifeNerd = new Audio('sounds/noLifeNerd.mp3')
 var back = document.getElementsByClassName("slidingBackground");
-
 var j;
 
 function changeImage() {
     for (j = 0; j < back.length; j++) {
         if (back[j].id == "slidingRatMonkaS") {
             back[j].setAttribute("id", "slidingRatChomp");
+            crackTime.play();
         }
         else if (back[j].id == "slidingRatChomp") {
             back[j].setAttribute("id", "slidingRatMonkaS");
+            monkaS.play();
         }
         else if (back[j].id == "slidingPluto") {
             back[j].setAttribute("id", "slidingRad");
+            pogSFX.play();
         }
         else if (back[j].id == "slidingRad") {
             back[j].setAttribute("id", "slidingPluto");
+            noLifeNerd.play();
         }
     }
 }
